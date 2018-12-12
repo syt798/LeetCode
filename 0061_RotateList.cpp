720827f0b6721250cacb002158e26b59888bc8ac
SLazy <syttys798@163.com>
2018-12-12 19:55:55 +0800

    Create 0061_RotateList.cpp

---
+++ 0061_RotateList.cpp
@@ -0,0 +1,62 @@
+/*
+61.旋转链表
+给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+输入: 1->2->3->4->5->NULL, k = 2
+输出: 4->5->1->2->3->NULL
+解释:
+向右旋转 1 步: 5->1->2->3->4->NULL
+向右旋转 2 步: 4->5->1->2->3->NULL
+
+示例 2:
+输入: 0->1->2->NULL, k = 4
+输出: 2->0->1->NULL
+解释:
+向右旋转 1 步: 2->0->1->NULL
+向右旋转 2 步: 1->2->0->NULL
+向右旋转 3 步: 0->1->2->NULL
+向右旋转 4 步: 2->0->1->NULL
+
+作者:syt
+日期：2018-12-12
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+       if(head == NULL || k <= 0)
+            return head;
+        int len = 1;
+        ListNode *tail = head->next;
+        ListNode *tail_pre = head;
+        while(tail != NULL)
+        {
+            len++;
+            tail = tail->next;
+            tail_pre = tail_pre->next;
+        }
+       
+        tail_pre->next = head;
+        int steps = len - (k%len);
+        
+        while(steps > 0)
+        {
+            head = head->next;
+            tail_pre = tail_pre->next;
+            steps--;
+        }
+        
+        tail_pre->next = NULL;
+        return head;
+        
+    }
+};
