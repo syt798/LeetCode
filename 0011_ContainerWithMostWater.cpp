c3457cfe4b8af880795426f2a09738502a48e12f
SLazy <syttys798@163.com>
2018-12-19 10:10:09 +0800

    Create 0011_ContainerWithMostWater.cpp

---
+++ 0011_ContainerWithMostWater.cpp
@@ -0,0 +1,31 @@
+/*
+11.盛最多水的容器
+给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+说明：你不能倾斜容器，且 n 的值至少为 2。
+
+示例:
+输入: [1,8,6,2,5,4,8,3,7]
+输出: 49
+
+作者：syt
+日期：2018-12-19
+*/
+
+//思路：遍历计算每个节点与其它节点所能形成的最大的值
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max = 0;
+        for(int i = 0; i < height.size()-1; ++i)
+        {
+            for(int j = i+1; j < height.size(); ++j)
+            {
+                int border = height[i] > height[j]?height[j]:height[i];
+                int tmp = (j-i)*border;
+                if(tmp > max)
+                    max = tmp;
+            }
+        }
+        return max;
+    }
+};
